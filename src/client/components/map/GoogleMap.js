4f10dc9abd438da89b16cbcb78ff7d7b05e0bca7
Diego Marques <dmarquesdev@gmail.com>
2017-04-01 15:34:09 -0300

    Informações sensíveis utilizando config.js

--- src/client/components/map/GoogleMap.js
+++ src/client/components/map/GoogleMap.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
+import config from '../../../shared/config';
 import Marker from './Marker';
 
 const GoogleMap = ({ center, zoom, markers }) => {
   const markerList = markers.map((marker) => <Marker {...marker} />);
   return (
-    <GoogleMapReact center={center} zoom={zoom}>
+    <GoogleMapReact 
+		  center={center} 
+		  zoom={zoom}
+		  bootstrapURLKeys={{ key: config.GMAPS_API_KEY }}
+	>
       {markerList}
     </GoogleMapReact>
   );
