7d254cd1f9459f8b21482e45b45813ba9fe5665a
Diego Marques <dmarquesdev@gmail.com>
2017-04-01 16:00:32 -0300

    Correção no process.env

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -25,7 +25,7 @@ module.exports = {
 
 		plugins: [
 				new webpack.DefinePlugin({
-						'process_env': {
+						'process.env': {
 								NODE_ENV: JSON.stringify('production')
 						}
 				}), 
